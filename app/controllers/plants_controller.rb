583486af2dbd3f9e8d2568b4eaa19fdfb1e35a0a
lordish <gitonga403@gmail.com>
2023-06-20 18:58:30 +0300

    final commit

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,6 @@
 class PlantsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
@@ -8,14 +9,30 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
   # POST /plants
   def create
     plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    render json: plant
+  end
+
+  #Update /plants/:id
+
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  #Delete /plants/:id
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
@@ -23,4 +40,14 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+      render json: { error: "Plant not found" }, status: :not_found
+  end
+
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
 end
